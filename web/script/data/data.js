25e9e778ecb2b6598b9c30fe3d02ef5f8d585bc8
simplemx <simplemx@126.com>
2012-06-16 21:06:44 +0800

    add for refactoring js/css/html

--- web/script/data/data.js
+++
@@ -1,5 +0,0 @@
-﻿(function(context){
-	context = context && window;
-	var loadDataList = ["data1.js","data2.js","data3.js","data4.js","data5.js","data6.js","data7.js","data8.js","data9.js","data10.js"];
-	context.beginLoad && context.beginLoad(loadDataList);
-})(window);
